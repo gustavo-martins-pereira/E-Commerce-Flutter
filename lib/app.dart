e4ecfb9ef75b97a23da12893fca74f9166884995
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2025-06-12 18:10:59 -0300

    ADD - Utility Functions

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce/utils/themes/theme.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "E-Commerce",
+      themeMode: ThemeMode.system,
+      theme: CAppTheme.lightTheme,
+      darkTheme: CAppTheme.darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
-import 'package:ecommerce/utils/themes/theme.dart';
 import 'package:flutter/material.dart';
+
+import 'app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "E-Commerce",
-      themeMode: ThemeMode.system,
-      theme: CAppTheme.lightTheme,
-      darkTheme: CAppTheme.darkTheme,
-    );
-  }
 }
